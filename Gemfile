427724d3885affca43f312e237376bb2f8bec130
Bryan Anthonio <elorm9@gmail.com>
2012-08-08 20:30:27 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+
+# Bundle edge Rails instead:
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+
+group :development do
+    gem 'sqlite3', '1.3.5'
+end
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '74721e39f153cdb8ab69b20650441603f37589704ad6f37afc6889463fb073d109c76e3b510a9126f761ba6e0ed7000130c06914424e832d944374051cbba00d'
